93af068b80a11c59fc27e15834163f16733e7f82
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-11 16:19:04 +0900

    squash! Init Project with Github Action

--- src/test/java/com/example/spring3edutest/Spring3EduTestApplicationTests.java
+++ src/test/java/com/example/spring3edutest/Spring3EduTestApplicationTests.java
@@ -3,11 +3,20 @@ package com.example.spring3edutest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class Spring3EduTestApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test()
+    void FailTest() {
+
+        //given
+        fail("실패");
     }
 
 }
