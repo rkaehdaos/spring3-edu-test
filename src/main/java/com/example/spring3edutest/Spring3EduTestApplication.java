45828d0fe379c9d18d4e5d4f6641b6ff08cbdea6
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-11 14:46:39 +0900

    Init Project

---
+++ src/main/java/com/example/spring3edutest/Spring3EduTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring3edutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3EduTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3EduTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring3edutest/Spring3EduTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring3edutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3EduTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
