530220306d0d5c804080ae9aeeb323cb26d87003
Kai Ahn <kai.ahn@businesson.co.kr>
2024-04-03 11:20:17 +0900

    [#5 feat] slf4j log print

--- src/test/java/com/example/spring3edutest/Spring3EduTestApplicationTests.java
+++ src/test/java/com/example/spring3edutest/Spring3EduTestApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.spring3edutest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class Spring3EduTestApplicationTests {
     @Test
     void contextLoads() {
+        log.debug("Just Test");
     }
 }
